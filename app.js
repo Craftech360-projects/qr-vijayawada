9fa9837f78b701d1d5d8d35be21f7052bde81b73
Abilash <abilashs003@gmail.com>
2023-11-23 07:53:26 +0530

    updated changes

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ app.set("views", "views");
 
 const mongo_URI =
   "mongodb+srv://yamuna:Dbnd0ki7s3DC0DQ3@cluster0.v6kew10.mongodb.net/qr-28-11st";
-// Main Colletion for QR Scanning
+
 const usertwoSchema = new mongoose.Schema({
   code: {
     type: String,
@@ -157,6 +157,56 @@ async function generateAndStoreRandomNumber() {
   return formattedNumber;
 }
 
+async function sendEmail(email, name, randomNumber) {
+  const dynamicImageUrl = `https://raw.githubusercontent.com/Craftech360-projects/qr-vijayawada/new-qrCode/mainImages/${randomNumber}.png`;
+
+  const htmlString = `<!doctype html><html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office"><head><title>GOURMETLUXE</title><!--[if !mso]><!--><meta http-equiv="X-UA-Compatible" content="IE=edge"><!--<![endif]--><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><style type="text/css">#outlook a { padding:0; }
+  body { margin:0;padding:0;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%; }
+  table, td { border-collapse:collapse;mso-table-lspace:0pt;mso-table-rspace:0pt; }
+  img { border:0;height:auto;line-height:100%; outline:none;text-decoration:none;-ms-interpolation-mode:bicubic; }
+  p { display:block;margin:13px 0; }</style><!--[if mso]><noscript><xml><o:OfficeDocumentSettings><o:AllowPNG/>
+  <o:PixelsPerInch>96</o:PixelsPerInch></o:OfficeDocumentSettings></xml></noscript><![endif]--><!--[if lte mso 11]><style type="text/css">
+  .mj-outlook-group-fix { width:100% !important; }</style><![endif]--><style type="text/css">@media only screen and (min-width:480px) {
+    .mj-column-per-100 { width:100% !important; max-width: 100%; }
+  }</style><style media="screen and (min-width:480px)">.moz-text-html .mj-column-per-100 { width:100% !important; max-width: 100%; }</style><style type="text/css">@media only screen and (max-width:479px) {
+  table.mj-full-width-mobile { width: 100% !important; }
+  td.mj-full-width-mobile { width: auto !important; }
+  }</style><style type="text/css"></style></head><body style="word-spacing:normal;">
+  <div><!--[if mso | IE]><table align="center" border="0" cellpadding="0" cellspacing="0" class="" role="presentation" style="width:600px;" width="600" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]--><div style="margin:0px auto;max-width:600px;"><table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="width:100%;"><tbody><tr><td style="direction:ltr;font-size:0px;padding:20px 0;text-align:center;"><!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td class="" style="vertical-align:top;width:600px;" ><![endif]--><div class="mj-column-per-100 mj-outlook-group-fix" style="font-size:0px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;"><table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:top;" width="100%"><tbody><tr><td align="center" style="font-size:0px;padding:10px 25px;word-break:break-word;"><table border="0" cellpadding="0" cellspacing="0" role="presentation" style="border-collapse:collapse;border-spacing:0px;"><tbody><tr><td style="width:500px;">
+  <img alt="Full Screen Image" src="https://raw.githubusercontent.com/Craftech360-projects/qr-vijayawada/new-qrCode/mainImages/SYMPH${randomNumber}.png" style="border:0;display:block;outline:none;text-decoration:none;height:auto;width:100%;font-size:13px;" width="500" height="auto"></td></tr></tbody></table></td></tr></tbody></table></div><!--[if mso | IE]></td></tr></table><![endif]--></td></tr></tbody></table></div><!--[if mso | IE]></td></tr></table><![endif]--></div></body></html>`;
+
+  const Mailjet = require("node-mailjet");
+  const mailjet = Mailjet.apiConnect(
+    "f8c199da7a532fcb35fc83fce0e9ec55",
+    "60d75ea08bc4ff13fa68f30b531d6942"
+  );
+
+  try {
+    const request = await mailjet.post("send", { version: "v3.1" }).request({
+      Messages: [
+        {
+          From: {
+            Email: "invite@symphony2023events.com",
+            Name: "SYMPHONY EVENTS",
+          },
+          To: [
+            {
+              Email: email,
+              Name: name,
+            },
+          ],
+          Subject: "QR Code for SYMPHONY events",
+          HTMLPart: htmlString,
+        },
+      ],
+    });
+
+    console.log(request.body);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 app.post("/generate", async (req, res) => {
   const existingUser = await User.findOne({ email: req.body.email });
 
@@ -165,35 +215,35 @@ app.post("/generate", async (req, res) => {
     console.log(
       `User already exists. Existing random number: ${existingRandomNumber}`
     );
+    await sendEmail(req.body.email, req.body.name, existingRandomNumber);
     const dynamicImageUrl = `https://raw.githubusercontent.com/Craftech360-projects/qr-vijayawada/new-qrCode/mainImages/SYMPH${existingRandomNumber}.png`;
     io.emit("emitUrl", dynamicImageUrl);
-    return res
-      .status(200)
-      .json({ message: "User already exists. Existing QR code sent." });
-  }
+    // return res
+    //   .status(200)
+    //   .json({ message: "User already exists. Existing QR code sent." });
+  } else {
+    let email = req.body.email;
+    let nameValue = req.body.name;
+    // generate random qr code
+    const randomNumber = await generateAndStoreRandomNumber();
 
-  let email = req.body.email;
-  let nameValue = req.body.name;
-  // generate random qr code
-  const randomNumber = await generateAndStoreRandomNumber();
-
-  const newUser = new User({
-    code: `SYMPH${randomNumber}`,
-    email: email,
-    name: nameValue,
-  });
-
-  newUser
-    .save()
-    .then(() => {})
-    .catch((error) => {
-      console.error("Error saving to MongoDB:", error);
-      res.status(500).json({ message: "Failed to register user" });
+    const newUser = new User({
+      code: `SYMPH${randomNumber}`,
+      email: email,
+      name: nameValue,
     });
 
-  const dynamicImageUrl = `https://raw.githubusercontent.com/Craftech360-projects/qr-vijayawada/new-qrCode/mainImages/${randomNumber}.png`;
+    newUser
+      .save()
+      .then(() => {})
+      .catch((error) => {
+        console.error("Error saving to MongoDB:", error);
+        res.status(500).json({ message: "Failed to register user" });
+      });
 
-  var htmlString = `<!doctype html><html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office"><head><title>GOURMETLUXE</title><!--[if !mso]><!--><meta http-equiv="X-UA-Compatible" content="IE=edge"><!--<![endif]--><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><style type="text/css">#outlook a { padding:0; }
+    const dynamicImageUrl = `https://raw.githubusercontent.com/Craftech360-projects/qr-vijayawada/new-qrCode/mainImages/${randomNumber}.png`;
+
+    var htmlString = `<!doctype html><html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office"><head><title>GOURMETLUXE</title><!--[if !mso]><!--><meta http-equiv="X-UA-Compatible" content="IE=edge"><!--<![endif]--><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><style type="text/css">#outlook a { padding:0; }
         body { margin:0;padding:0;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%; }
         table, td { border-collapse:collapse;mso-table-lspace:0pt;mso-table-rspace:0pt; }
         img { border:0;height:auto;line-height:100%; outline:none;text-decoration:none;-ms-interpolation-mode:bicubic; }
@@ -217,42 +267,43 @@ app.post("/generate", async (req, res) => {
         td.mj-full-width-mobile { width: auto !important; }
       }</style><style type="text/css"></style></head><body style="word-spacing:normal;">
       <div><!--[if mso | IE]><table align="center" border="0" cellpadding="0" cellspacing="0" class="" role="presentation" style="width:600px;" width="600" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]--><div style="margin:0px auto;max-width:600px;"><table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="width:100%;"><tbody><tr><td style="direction:ltr;font-size:0px;padding:20px 0;text-align:center;"><!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td class="" style="vertical-align:top;width:600px;" ><![endif]--><div class="mj-column-per-100 mj-outlook-group-fix" style="font-size:0px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;"><table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:top;" width="100%"><tbody><tr><td align="center" style="font-size:0px;padding:10px 25px;word-break:break-word;"><table border="0" cellpadding="0" cellspacing="0" role="presentation" style="border-collapse:collapse;border-spacing:0px;"><tbody><tr><td style="width:500px;">
-      <img alt="Full Screen Image" src="https://raw.githubusercontent.com/Craftech360-projects/qr-vijayawada/new-qrCode/mainImages/SYMPH0001.png" style="border:0;display:block;outline:none;text-decoration:none;height:auto;width:100%;font-size:13px;" width="500" height="auto"></td></tr></tbody></table></td></tr></tbody></table></div><!--[if mso | IE]></td></tr></table><![endif]--></td></tr></tbody></table></div><!--[if mso | IE]></td></tr></table><![endif]--></div></body></html>`;
+      <img alt="Full Screen Image" src="https://raw.githubusercontent.com/Craftech360-projects/qr-vijayawada/new-qrCode/mainImages/SYMPH${randomNumber}.png" style="border:0;display:block;outline:none;text-decoration:none;height:auto;width:100%;font-size:13px;" width="500" height="auto"></td></tr></tbody></table></td></tr></tbody></table></div><!--[if mso | IE]></td></tr></table><![endif]--></td></tr></tbody></table></div><!--[if mso | IE]></td></tr></table><![endif]--></div></body></html>`;
 
-  console.log(htmlString);
-  const Mailjet = require("node-mailjet");
-  const mailjet = Mailjet.apiConnect(
-    "f8c199da7a532fcb35fc83fce0e9ec55",
-    "60d75ea08bc4ff13fa68f30b531d6942"
-  );
+    console.log(htmlString);
+    const Mailjet = require("node-mailjet");
+    const mailjet = Mailjet.apiConnect(
+      "f8c199da7a532fcb35fc83fce0e9ec55",
+      "60d75ea08bc4ff13fa68f30b531d6942"
+    );
 
-  const request = mailjet.post("send", { version: "v3.1" }).request({
-    Messages: [
-      {
-        From: {
-          Email: "invite@symphony2023events.com",
-          Name: "SYMPHONY EVENTS",
-        },
-        To: [
-          {
-            Email: email,
-            Name: nameValue,
+    const request = mailjet.post("send", { version: "v3.1" }).request({
+      Messages: [
+        {
+          From: {
+            Email: "invite@symphony2023events.com",
+            Name: "SYMPHONY EVENTS",
           },
-        ],
-        Subject: "QR Code for SYMPHONY events",
-        HTMLPart: htmlString,
-      },
-    ],
-  });
-
-  request
-    .then((x) => {
-      console.log(x.body);
-      io.emit("emitUrl", dynamicImageUrl);
-    })
-    .catch((err) => {
-      console.log(err);
+          To: [
+            {
+              Email: email,
+              Name: nameValue,
+            },
+          ],
+          Subject: "QR Code for SYMPHONY events",
+          HTMLPart: htmlString,
+        },
+      ],
     });
+
+    request
+      .then((x) => {
+        console.log(x.body);
+        io.emit("emitUrl", dynamicImageUrl);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 });
 //Get users-list
 io.on("connection", (socket) => {
